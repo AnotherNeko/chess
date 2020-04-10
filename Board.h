27db34639ef207681acab316ba3e93f6f9a53847
Another Neko <samuelrempel@gmail.com>
2020-04-09 21:01:21 -0700

    added coordinates to commented chess board

--- Board.h
+++ Board.h
@@ -6,15 +6,15 @@ struct Board
 {
 	static Board* mainInstance;
     Board();
-	Pieces mypieces[8 * 8] = 
-    { empty, empty, empty, empty, empty, empty, empty, empty,	 //   ██  ██  ██  ██ black side
-      empty, empty, empty, empty, empty, empty, empty, empty,	 // ██  ██  ██  ██   black side
-      empty, empty, empty, empty, empty, empty, empty, empty,	 //   ██  ██  ██  ██
-      empty, empty, empty, empty, empty, empty, empty, empty,	 // ██  ██  ██  ██
-      empty, empty, empty, empty, empty, empty, empty, empty,	 //   ██  ██  ██  ██
-      empty, empty, empty, empty, empty, empty, empty, empty,	 // ██  ██  ██  ██
-      empty, empty, empty, empty, empty, empty, empty, empty,	 //   ██  ██  ██  ██ white side
-      empty, empty, empty, empty, empty, empty, empty, empty };  // ██  ██  ██  ██   white side
+	Pieces mypieces[8 * 8] =                                     //  A B C D E F G H 
+    { empty, empty, empty, empty, empty, empty, empty, empty,	 // 8   ██  ██  ██  ██ black side
+      empty, empty, empty, empty, empty, empty, empty, empty,	 // 7 ██  ██  ██  ██   black side
+      empty, empty, empty, empty, empty, empty, empty, empty,	 // 6   ██  ██  ██  ██
+      empty, empty, empty, empty, empty, empty, empty, empty,	 // 5 ██  ██  ██  ██
+      empty, empty, empty, empty, empty, empty, empty, empty,	 // 4   ██  ██  ██  ██
+      empty, empty, empty, empty, empty, empty, empty, empty,	 // 3 ██  ██  ██  ██
+      empty, empty, empty, empty, empty, empty, empty, empty,	 // 2   ██  ██  ██  ██ white side
+      empty, empty, empty, empty, empty, empty, empty, empty };  // 1 ██  ██  ██  ██   white side
    
     void forceMove(Pos2 piece, Pos2 to);
     void move(Pos2 piece, Pos2 to);
